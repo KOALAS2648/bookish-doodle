58d9b0c091a008d16c67d63bead0ab6ce43ecb49
OliverDevon <oliverdevon@icloud.com>
2025-10-05 20:36:32 +0100

    fix my error

--- box.py
+++ box.py
@@ -9,10 +9,13 @@ class Box:
         self.centralx = self.x-self.width//2
         self.centraly = self.y-self.height//2
         self.color = (255, 0, 0)
-
+    def update_pos(self):
+        self.centralx = self.x-self.width//2
+        self.centraly = self.y-self.height//2
     def check_collision(self, other):
-        check_collision_y = other.y+other.height >= self.y
-        check_collision_x = other.x+other.width >= self.x 
+        check_collision_y = other.centraly+other.height >= self.centraly and other.centraly <= self.centraly + self.height
+        check_collision_x = other.centralx+other.width >= self.centralx and other.centralx <= self.centralx+self.width
         return check_collision_y and check_collision_x
     def draw(self, screen):
+        
         pg.draw.rect(screen, self.color, (self.centralx, self.centraly, self.width, self.height))
